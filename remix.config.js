a59608347b6409601966c6cad1d01c86f6757a19
Mark Mulvaney <mark@bambwa.com>
2023-02-27 14:16:22 +0100

    updates to config for deployment

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? './server.js'
+      : undefined,
+  serverBuildPath: '.netlify/functions-internal/server.js',
   future: {
     unstable_tailwind: true,
   },
-}
+};
