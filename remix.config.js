04d6382da636949c0803756c076ed4211845c5b2
Mark Mulvaney <mark@bambwa.com>
2023-02-27 16:13:05 +0100

    removing netlify

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
-  server:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? './server.js'
-      : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
+
   future: {
     unstable_tailwind: true,
   },
