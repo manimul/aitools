2554da1a585cdfe890c7d97ef83042e149c37e79
Mark Mulvaney <mark@bambwa.com>
2023-03-06 16:15:37 +0100

    updates to netlify toml

--- remix.config.js
+++ remix.config.js
@@ -3,16 +3,16 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   appDirectory: 'app',
-  server:
-    process.env.NETLIFY || process.env.NETLIFY_LOCAL
-      ? './server.js'
-      : undefined,
-  serverBuildPath: '.netlify/functions-internal/server.js',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
   serverBuildDirectory: 'build',
   devServerPort: 8002,
   ignoredRouteFiles: ['**/.*'],
+  server:
+    process.env.NETLIFY || process.env.NETLIFY_LOCAL
+      ? './server.js'
+      : undefined,
+  serverBuildPath: '.netlify/functions-internal/server.js',
 
   future: {
     unstable_tailwind: true,
